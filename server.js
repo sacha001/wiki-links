0abb79c50cb62ad7c3dcc669b8908e1ed5a2b56b
Sacha Silance <ssilance@unb.ca>
2020-11-30 22:53:37 -0400

    Begin API calls

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
 
-app.get('/test', function (req, res) {
-    res
-    .status(200)
-    .send('response');
+app.get('/findlinks', function (req, res) {
+    let page1 = req.query.page1;
+    let page2 = req.query.page2;
+    console.log(`Fetching: ${page1}`);
+    axios.get(`https://en.wikipedia.org/w/api.php?action=parse&page=${page1}&prop=links&formatversion=2&format=json`).then((response) => {
+        res
+        .status(200)
+        .send(JSON.stringify(response));
+    })
+    .catch(function (error) {
+        console.log(error);
+        res
+        .status(500)
+        .send(error);
+    });
 });
 
 
